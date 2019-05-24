a54e4836e14c8237ad0c4f344ab0c11039d1d3c0
Alberto Vicens <avicens@Air-de-Alberto.lan>
2019-05-24 10:09:41 +0200

    Get clone tree from pyclone outputs

---
+++ scripts/clone_tree_inference_clonevol.R
@@ -0,0 +1,72 @@
+library(clonevol)
+
+pyclone.output.dirs<-list.dirs(path = "data/pyclone_output",recursive = F)
+
+for (i in 1:length(pyclone.output.dirs)) {
+
+  sample<-sapply(strsplit(pyclone.output.dirs[i],"/"),"[",3)
+  pyclone.output.dir<-pyclone.output.dirs[i]
+  pyclone.loci<-read.table(paste(pyclone.output.dir,"tables/loci.tsv",sep="/"),
+                         sep="\t", header=T, stringsAsFactors = F)
+
+##Only for multiple samples
+#Replicate variant frequency column with sample name
+#vaf.col.names <- grep('.vaf', colnames(pyclone.loci), value=T)
+#sample.names <- gsub('.vaf', '', vaf.col.names)
+#pyclone.loci[,sample.names] <- pyclone.loci[, vaf.col.names]
+#vaf.col.names <- sample.names
+
+#pyclone.loci[,sample.names]<-pyclone.loci$variant_allele_frequency
+#names(pyclone.loci)[6]<-paste(sample.names,"vaf",sep=".")
+
+#sample.groups<-sample.names
+#names(sample.groups)<-vaf.col.names
+
+#Order data frame by cluster numbering
+  pyclone.loci<-pyclone.loci[order(pyclone.loci$cluster_id,decreasing = F),]
+
+#Discard clusters with < 3 mutations
+  cluster.freq<-table(pyclone.loci$cluster_id)
+
+  for (i in 1:length(cluster.freq)) {
+    if (cluster.freq[i] < 3) {
+      pyclone.loci<-subset(pyclone.loci, subset= cluster_id != i)
+    }
+  } 
+
+#Add column with normalized cluster ID
+  cluster.freq2<-table(pyclone.loci$cluster_id)
+  cluster_id_norm<-integer()
+
+  i<-1
+  while(i <=  length(cluster.freq2)) {
+    cluster_id_norm<-c(cluster_id_norm,rep(i, cluster.freq2[i]))
+    i= i+1
+    }
+
+  pyclone.loci$cluster<-cluster_id_norm
+
+  clone.tree = infer.clonal.models(variants = pyclone.loci,
+                        cluster.col.name = 'cluster',
+                        vaf.col.names = 'cellular_prevalence',
+                        cancer.initiation.model='monoclonal',
+                        subclonal.test = 'bootstrap',
+                        subclonal.test.model = 'non-parametric',
+                        num.boots = 1000,
+                        founding.cluster = order(table(pyclone.loci$cluster), decreasing = T)[1],
+                        cluster.center = 'mean',
+                        ignore.clusters = NULL,
+                        min.cluster.vaf = 0.01,
+                        sum.p = 0.05,
+                        alpha = 0.05)
+
+  clone.tree.df<-clone.tree$matched$merged.trees[[1]]
+#clone.tree.branch <- convert.consensus.tree.clone.to.branch(clone.tree.df, branch.scale = "sqrt")
+
+  if (!file.exists(paste(pyclone.output.dir,"tree",sep="/"))) {
+    dir.create(paste(pyclone.output.dir,"tree",sep="/"))
+  }
+
+  write.table(clone.tree.df,paste(pyclone.output.dir,"/tree/",sample,"_pyclone_tree.tsv",sep=""),
+              sep="\t",row.names = F, col.names = T, quote = F)
+}
