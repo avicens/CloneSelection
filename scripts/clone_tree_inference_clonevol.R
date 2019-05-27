44685566e8fd251d2505a806e0b0b72312e5151f
avicens <chechu086@gmail.com>
2019-05-27 16:46:47 +0200

    command line args[1]

--- scripts/clone_tree_inference_clonevol.R
+++ scripts/clone_tree_inference_clonevol.R
@@ -1,10 +1,79 @@
+#Input arguments
+args<-commandArgs(TRUE)
+pyclone_out_dir<-args[1]
+
 library(clonevol)
 
-pyclone.output.dirs<-list.dirs(path = "data/pyclone_output",recursive = F)
+pyclone.output.dirs<-list.dirs(path = pyclone_out_dir,recursive = F)
+
+#Function convert.clone.to.branch
+convert.clone.to.branch <- function(t, branch.lens = NULL,
+                                    merged.tree.node.annotation='sample.with.nonzero.cell.frac.ci'){
+  # symbols used for each branch
+  syms = c(seq(1,9), unlist(strsplit('abcdefghijklmnopqrstuvwxyz', '')))
+  t = t[!is.na(t$parent) & !is.na(t$excluded) & !t$excluded,]
+  t$branches = NA
+  t$blengths = NA
+  rownames(t) = t$lab
+  grow.tree <- function(t, lab, parent.symbol=''){
+    #print(paste0('---', lab))
+    if (t[lab,'parent'] == '-1'){
+      t[lab,'branches'] = 'Y'
+    }
+    children = !is.na(t$parent) & t$parent == lab
+    if (any(children)){
+      children.labs = t$lab[children]
+      num.children = length(children.labs)
+      if (num.children == 1){# && parent.symbol != ''){
+        # one child, grow straight branch (no left right)
+        children.syms = '0'
+      }else{
+        children.syms = syms[1:num.children]
+      }
+      children.syms = paste0(parent.symbol, children.syms)
+      t$branches[children] = children.syms
+      #print(children.labs)
+      #print(children.syms)
+      for (i in 1:length(children.labs)){
+        t = grow.tree(t, children.labs[i], children.syms[i])
+      }
+    }
+    return(t)
+  }
+  tg = grow.tree(t, t$lab[!is.na(t$parent) & t$parent == '-1'])
+  if (merged.tree.node.annotation=='sample.with.nonzero.cell.frac.ci'){
+    # remove ci info from sample annotation
+    tg$samples.with.nonzero.cell.frac = gsub(',+$', '',
+                                             gsub('\\s*:\\s*[^:]+(,|$)', ',', tg$sample.with.nonzero.cell.frac.ci))
+    #tg$samples.with.nonzero.cell.frac = gsub(',+$', '', gsub('\u00B0[^,]+(,|$)', '',
+    #     gsub('\\s*:\\s*[^:]+(,|$)', ',', tg$sample.with.cell.frac.ci)))
+  }else{
+    cat(paste0('WARN: merged.tree.node.annotation = ',
+               merged.tree.node.annotation, ' not supported! No node annotation made.\n'))
+  }
+  if (is.null(branch.lens)){
+    tg$blengths = 5
+  }else{
+    tg$blengths = branch.lens[tg$lab]
+  }
+  # color founding clone of met with diff. border
+  #tg$node.border.color = ifelse(
+  #    grepl('*', gsub('*P', '', tg[[merged.tree.node.annotation]], fixed=TRUE), fixed=T),
+  #    'red', 'black')
+  tg$node.border.color = 'black'
+  tg$node.border.width = 1
+  tg$branch.border.color = 'white'
+  tg$branch.border.linetype = 'solid'
+  tg$branch.border.width = 0.5
+  
+  return(tg)
+  
+}
+
 
 for (i in 1:length(pyclone.output.dirs)) {
 
-  sample<-sapply(strsplit(pyclone.output.dirs[i],"/"),"[",3)
+  sample<-sapply(strsplit(pyclone.output.dirs[i],"/"),"[",4)
   pyclone.output.dir<-pyclone.output.dirs[i]
   pyclone.loci<-read.table(paste(pyclone.output.dir,"tables/loci.tsv",sep="/"),
                          sep="\t", header=T, stringsAsFactors = F)
@@ -63,6 +132,7 @@ for (i in 1:length(pyclone.output.dirs)) {
                         min.cluster.vaf = 0.01,
                         sum.p = 0.05,
                         alpha = 0.05)
+  
 
   clone.tree.branched<-convert.clone.to.branch(clone.tree$matched$merged.trees[[1]], 
                                                branch.lens=NULL, merged.tree.node.annotation = "")
