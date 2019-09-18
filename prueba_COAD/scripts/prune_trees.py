d53305fc952a653a99f6d8dc27dfc0b6e949fd66
avicens <chechu086@gmail.com>
2019-09-18 10:13:23 +0200

    add  paths and sample as input argument

--- prueba_COAD/scripts/prune_trees.py
+++ prueba_COAD/scripts/prune_trees.py
@@ -1,10 +1,22 @@
 from ete3 import Tree
+import sys
 
-rt = Tree("/Users/avicens/Dropbox/proyectos/dNdS_clones/prueba_COAD/trace/raxml_trees/rooted/TCGA-4N-A93T_rooted.raxml.bestTree", format=0)
+working_dir="/Users/avicens/Dropbox/proyectos/dNdS_clones/prueba_COAD/"
+tree_dir = working_dir + "trace/raxml_trees/rooted/"
+
+program_name = sys.argv[0]
+sample_name = sys.argv[1]
+#Example: sample_name="TCGA-4N-A93T"
+
+if len(sys.argv) != 2:
+    print ("Usage: prune_trees.py <sample_name>")
+    sys.exit(1)
+
+#rt = Tree("/Users/avicens/Dropbox/proyectos/dNdS_clones/prueba_COAD/trace/raxml_trees/rooted/TCGA-4N-A93T_rooted.raxml.bestTree", format=0)
+rt = Tree(tree_dir + sample_name + "_rooted.raxml.bestTree")
 
 #Prune tree maintaining clone leaves
-
-#Ej: clone_leaves = ["clone1", "clone2", "clone3"]
+#Example: clone_leaves = ["clone1", "clone2", "clone3"]
 
 rt.prune(rt.get_leaves()[:-1])
 
