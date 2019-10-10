841078258742155a31b8fdc21e83752ebf94b493
avicens <chechu086@gmail.com>
2019-10-10 14:16:25 +0200

    Fitted todndscv_samples.R script

---
+++ prueba_COAD/scripts/run_dndscv_array.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --mail-type=BEGIN,FAIL,TIME_LIMIT_80,TIME_LIMIT,END
+#SBATCH --mail-user=avicens@uvigo.es
+#SBATCH -J dndscv_bulk 
+#SBATCH -o %x-%A_%a.out
+#SBATCH --time=00:10:00
+#SBATCH -n 1
+#SBATCH -p shared --qos=shared
+#SBATCH -c 2
+#SBATCH --mem-per-cpu=10GB
+
+#source readConfig.sh $1
+
+WORKDIR="/home/uvi/be/avs/store/dNdS_clones/prueba_COAD"
+DATADIR="$WORKDIR/data"
+SCRIPTDIR="$WORKDIR/scripts"
+SAMPLEDIR="$WORKDIR/samples"
+
+SAMPLES="$SAMPLEDIR/COAD_samples.txt"
+
+module load gcc/6.4.0 R/3.5.3
+
+mkdir -p $DATADIR/dndscv
+
+SAMPLE=`sed "${SLURM_ARRAY_TASK_ID}q;d" ${SAMPLES}`
+
+mkdir -p $DATADIR/dndscv/${SAMPLE}
+echo "Running dNdScv script for  ${SAMPLE}"
+SNV="$DATADIR/snv/${SAMPLE}_SNV.tsv.gz"
+LOCI="$DATADIR/pyclone/loci/${SAMPLE}_loci.txt"
+INFILE="$DATADIR/dndscv/${SAMPLE}/${SAMPLE}_dndscv_input.txt"
+OUTFILE="$DATADIR/dndscv/${SAMPLE}/${SAMPLE}_bulk_dnds.txt"
+
+Rscript $SCRIPTDIR/dndscv_samples.R $SNV $SAMPLE $INFILE $OUTFILE
+
+
