0beba91b144194e5efcd4c84fbaa12ffd596a3fb
avicens <chechu086@gmail.com>
2019-11-07 13:36:30 +0100

    Added input arguments

--- prueba_COAD/scripts/get_clusters_ctpsingle.py
+++ prueba_COAD/scripts/get_clusters_ctpsingle.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import pandas as pd
 
-cwd=os.getcwd()
-dirs = os.listdir(os.path.join(cwd,'data','ctpsingle'))
+ctpdir = sys.argv[1]
+num_clusters_file=sys.argv[2]
+cluster_size_file=sys.argv[3]
+
+#ctpdir = os.path.join(cwd,'data','ctpsingle','ctpsingle_output2')
+dirs = os.listdir(ctpdir)
 samples = [s for s in dirs if "TCGA" in s]
 
 #Open files
-num_clusters_file=os.path.join(cwd,'data','ctpsingle','num_clusters_per_sample.tsv')
+#num_clusters_file=os.path.join(ctpdir,'num_clusters_per_sample.tsv')
 fh=open(num_clusters_file,'w')
 fh.write("sample\tclusters\n")
 
-cluster_size_file = os.path.join(cwd,'data','ctpsingle','clusters_size_per_sample.tsv')
+#cluster_size_file = os.path.join(ctpdir,'clusters_size_per_sample.tsv')
 fi=open(cluster_size_file,'w')
 fi.write("sample\tcluster\tsize\n")
 
 for sample in samples:
-    
-    input_path=os.path.join(cwd,'data','ctpsingle',sample,(sample + "_ctpsingle_cluster_assignments.txt"))
+    input_path=os.path.join(ctpdir,sample,(sample + "_ctpsingle_cluster_assignments.txt"))
     if os.path.exists(input_path):
         clusters_file=pd.read_csv(input_path, sep='\t')
-    
         #Add lines with number of clusters per sample
         num_clusters=clusters_file['mostLikely'].nunique()
         print(sample+"\t"+str(num_clusters), file=fh)
-
-        #Add lines with sizes of clusters from each samp    le
+        #Add lines with sizes of clusters from each sample
         cluster_size=clusters_file['mostLikely'].value_counts()
         cluster_size_df=cluster_size.to_frame()
         cluster_size_df.columns = ['size']
